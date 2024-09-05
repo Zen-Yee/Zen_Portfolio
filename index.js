0ba6756734915b7582a7578fb15e1b2b2d64bd24
Zen-Yee <qhui.yee@gmail.com>
2024-09-05 21:51:39 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 /*EVENT LISTENER*/
 /* --------NAVIGATION BAR --------*/
-document.querySelector(".icon").addEventListener("click", addResponsive);
-
-/*FUNCTION*/
-function addResponsive(){
+document.querySelector(".icon").addEventListener("click", function(){
     navBar = document.getElementById("topnav");
     if(navBar.className === "navTop"){
         navBar.classList.add("responsive");
     } else {
         navBar.classList.remove("responsive");
     }
-}
+})
+
+document.querySelector("#backCard-toggle").addEventListener("click", function(){
+    
+})
