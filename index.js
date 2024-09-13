fd1bb5bf3012267e62282bdd27f8ace1eacbf6c1
Zen-Yee <qhui.yee@gmail.com>
2024-09-13 15:31:48 +0800

    update flip card animation

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ document.querySelector(".icon").addEventListener("click", function(){
     }
 })
 
-document.querySelector("#backCard-toggle").addEventListener("click", function(){
+
+const flipBtn=document.querySelector(".btn-aboutMe");
+flipBtn.addEventListener('click',function(){
+    FrontC = document.querySelector(".frontCard");
+    BackC = document.querySelector(".backCard");
+    if(FrontC.classList.contains("hidden")){
+        FrontC.classList.remove("hidden");
+        BackC.classList.add("hidden");
+        flipBtn.innerHTML="About Me";
+    }else{
+        FrontC.classList.add("hidden");
+        BackC.classList.remove("hidden");
+        flipBtn.innerHTML="Back";
+    }
     
 })
+
+/* -------- FLIP CARD -----------
+const flipBtn=document.getElementsByClassName("btn-aboutMe");
+
+for(var i=0;i<flipBtn.length;i++){
+    flipBtn[i].addEventListener('click',function(){
+       FrontC = document.querySelector(".frontCard");
+       BackC = document.querySelector(".backCard");
+       FrontC.classList.toggle("hidden");
+       BackC.classList.toggle("hidden");
+       flipBtn.innerHTML;
+    });
+}
+*/
